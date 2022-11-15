e4ef4d59134b617fd349224d00a348be4ba87d17
hisho <hisho.web@gmail.com>
2022-11-15 17:08:27 +0900

    🔐 env追加

---
+++ frontend/src/constant/env.ts
@@ -0,0 +1,5 @@
+export const NEXT_PUBLIC_FRONTEND_URL = process.env || 'http://localhost:3000'
+export const NEXT_PUBLIC_BACKEND_URL =
+  process.env['NEXT_PUBLIC_BACKEND_URL'] || 'http://localhost:4000'
+export const NEXT_PUBLIC_GRAPHQL_URL =
+  process.env['NEXT_PUBLIC_GRAPHQL_URL'] || 'http://localhost:4000/graphql'
