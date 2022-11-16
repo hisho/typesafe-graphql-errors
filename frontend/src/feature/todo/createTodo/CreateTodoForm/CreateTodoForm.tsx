2bfd18510c707094a331d1771ddd38ee2e5d726a
hisho <hisho.web@gmail.com>
2022-11-16 19:14:27 +0900

    ✨ CreateTodoFormにReactHookFormを組み込み

--- frontend/src/feature/todo/createTodo/CreateTodoForm/CreateTodoForm.tsx
+++ frontend/src/feature/todo/createTodo/CreateTodoForm/CreateTodoForm.tsx
@@ -1,12 +1,34 @@
 import { Box, Button, Center, chakra, Heading, Input } from '@chakra-ui/react'
 import { FormControl } from '@src/component/FormControl/FormControl'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+import type { ToZod } from '@src/lib/zod/types'
+import type { CreateTodoInput } from '@src/lib/graphql/generate/graphql'
+
+const schema = z.object<ToZod<CreateTodoInput>>({
+  title: z.string(),
+  description: z.string(),
+})
 
 export const CreateTodoForm = () => {
+  const { handleSubmit } = useForm<CreateTodoInput>({
+    defaultValues: {
+      title: '',
+      description: '',
+    },
+    resolver: zodResolver(schema),
+  })
+
+  const handleCreateTodo = (data: CreateTodoInput) => {
+    console.log(data)
+  }
+
   return (
     <Box borderWidth={1} p={2}>
       <Heading>Create TODO</Heading>
       <Box h={4} />
-      <chakra.form>
+      <chakra.form onSubmit={handleSubmit(handleCreateTodo)}>
         <FormControl label={'タイトル'} isRequired>
           <Input />
         </FormControl>
