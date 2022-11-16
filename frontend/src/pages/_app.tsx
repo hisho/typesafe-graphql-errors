d57a90c6d2c2e5e63fef4a245f92642700fabb0d
hisho <hisho.web@gmail.com>
2022-11-16 17:41:56 +0900

    ✨ chakra uiを導入

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,10 +1,14 @@
 import type { AppProps } from 'next/app'
 import { GraphQLProvider } from '@src/feature/graphql/GraphQLProvider/GraphQLProvider'
+import { theme } from '@chakra-ui/react'
+import { ChakraProvider } from '@chakra-ui/provider'
 
 const App = ({ Component, pageProps }: AppProps) => {
   return (
     <GraphQLProvider>
-      <Component {...pageProps} />
+      <ChakraProvider theme={theme}>
+        <Component {...pageProps} />
+      </ChakraProvider>
     </GraphQLProvider>
   )
 }
