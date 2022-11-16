df72b3ca60ba8b56e2ea5c788d75970d4f128ead
hisho <hisho.web@gmail.com>
2022-11-16 16:41:54 +0900

    🔧 codegenの設定を更新

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -11,12 +11,23 @@ const sharedConfig = {
     createdAt: 'string',
     updatedAt: 'string',
   },
+  /**
+   * 入力のみavoidOptionalsを無効化する
+   * @see https://zenn.dev/izumin/articles/ffc84c1b4310be#avoidoptionals%3A-undefined-%E3%82%92%E5%8E%B3%E5%AF%86%E3%81%AB%E6%89%B1%E3%81%86
+   */
+  avoidOptionals: {
+    field: true,
+    inputValue: false,
+    object: true,
+    defaultValue: false,
+  },
 }
 
 const config: CodegenConfig = {
   overwrite: true,
   schema: process.env['NEXT_PUBLIC_GRAPHQL_URL'] ?? '',
   documents: ['src/**/*.graphql'],
+  ignoreNoDocuments: true,
   generates: {
     'src/lib/graphql/graphql.ts': {
       plugins: ['typescript'],
@@ -26,7 +37,7 @@ const config: CodegenConfig = {
       preset: 'near-operation-file',
       config: sharedConfig,
       presetConfig: {
-        extension: '.graphql.ts',
+        extension: '.generated.graphql.ts',
         baseTypesPath: '~@src/lib/graphql/graphql',
       },
       plugins: ['typescript-operations', 'typed-document-node'],
