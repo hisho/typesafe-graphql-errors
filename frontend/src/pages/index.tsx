4952364c114acf7420d149c03bef276e189918e0
hisho <hisho.web@gmail.com>
2022-11-16 20:05:40 +0900

    ✨ pageにCreateTodoFormを読み込ませた

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,5 +1,12 @@
+import { Container } from '@chakra-ui/react'
+import { CreateTodoForm } from '@src/feature/todo/createTodo/CreateTodoForm/CreateTodoForm'
+
 const Page = () => {
-  return <div>Next.js</div>
+  return (
+    <Container>
+      <CreateTodoForm />
+    </Container>
+  )
 }
 
 export default Page
