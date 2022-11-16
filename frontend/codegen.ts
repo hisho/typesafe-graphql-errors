3cbfa37dc57ef7a92e90b5e20ca433c03ed3dc86
hisho <hisho.web@gmail.com>
2022-11-16 17:42:46 +0900

    🔧 codegenの設定を変更

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -41,6 +41,9 @@ const config: CodegenConfig = {
         afterOneFileWrite: ['eslint --fix', 'prettier --write'],
       },
     },
+    './schema.graphql': {
+      plugins: ['schema-ast'],
+    },
   },
 }
 
