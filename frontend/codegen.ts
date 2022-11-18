ff0a846f752f295d3e3c19513689ea256471eb03
hisho <hisho.web@gmail.com>
2022-11-18 15:44:57 +0900

    🔧 avoidOptionalsをtrueに変更

--- frontend/codegen.ts
+++ frontend/codegen.ts
@@ -11,16 +11,7 @@ const sharedConfig = {
     createdAt: 'string',
     updatedAt: 'string',
   },
-  /**
-   * 入力のみavoidOptionalsを無効化する
-   * @see https://zenn.dev/izumin/articles/ffc84c1b4310be#avoidoptionals%3A-undefined-%E3%82%92%E5%8E%B3%E5%AF%86%E3%81%AB%E6%89%B1%E3%81%86
-   */
-  avoidOptionals: {
-    field: true,
-    inputValue: false,
-    object: true,
-    defaultValue: false,
-  },
+  avoidOptionals: true,
 }
 
 const config: CodegenConfig = {
