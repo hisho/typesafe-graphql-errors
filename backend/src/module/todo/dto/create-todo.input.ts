f1ad25b4599302952c2bf90c90ac5592b9aa739b
hisho <hisho.web@gmail.com>
2022-11-18 19:21:08 +0900

    ✨ create-todo.inputにdefaultValueを設定

--- backend/src/module/todo/dto/create-todo.input.ts
+++ backend/src/module/todo/dto/create-todo.input.ts
@@ -10,6 +10,7 @@ export class CreateTodoInput {
   @Field(() => String, {
     description: '説明',
     nullable: true,
+    defaultValue: null,
   })
   @IsOptional()
   @MinLength(10, { message: '説明文は10文字以上必要です。' })
