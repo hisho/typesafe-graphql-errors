65502b1a84533d9a9b6f983caea453dca80fedd1
hisho <hisho.web@gmail.com>
2022-11-18 17:09:48 +0900

    ✨ CreateTodoFormを動くように組み込み

--- frontend/src/feature/todo/createTodo/CreateTodoForm/CreateTodoForm.tsx
+++ frontend/src/feature/todo/createTodo/CreateTodoForm/CreateTodoForm.tsx
@@ -5,14 +5,31 @@ import { zodResolver } from '@hookform/resolvers/zod'
 import { z } from 'zod'
 import type { ToZod } from '@src/lib/zod/types'
 import type { CreateTodoInput } from '@src/lib/graphql/generate/graphql'
+import { gql, useMutation } from 'urql'
+import { useGraphQLErrors } from '@src/feature/graphql/customErrors/useCustomGraphqlErrors'
+import { isEmpty } from 'typesafe-utils'
 
-const schema = z.object<ToZod<CreateTodoInput>>({
-  title: z.string(),
-  description: z.string(),
-})
+const CreateTodoMutation = gql(/* GraphQL */ `
+  mutation createTodo($input: CreateTodoInput!) {
+    createTodo(input: $input) {
+      uuid
+    }
+  }
+`)
+
+const schema = z
+  .object<ToZod<CreateTodoInput>>({
+    title: z.string(),
+    description: z.string().nullable(),
+  })
+  .transform((value) => {
+    const description = isEmpty(value.description) ? null : value.description
+
+    return { title: value.title, description }
+  })
 
 export const CreateTodoForm = () => {
-  const { handleSubmit } = useForm<CreateTodoInput>({
+  const { handleSubmit, register, reset } = useForm<CreateTodoInput>({
     defaultValues: {
       title: '',
       description: '',
@@ -20,8 +37,27 @@ export const CreateTodoForm = () => {
     resolver: zodResolver(schema),
   })
 
-  const handleCreateTodo = (data: CreateTodoInput) => {
-    console.log(data)
+  const [{ fetching }, createTodo] = useMutation(CreateTodoMutation)
+  const { setErrors, findErrorMessages, resetErrors } =
+    useGraphQLErrors<CreateTodoInput>()
+
+  const handleCreateTodo = async (input: CreateTodoInput) => {
+    try {
+      const { error, data } = await createTodo({
+        input: {
+          title: input.title,
+          description: input.description,
+        },
+      })
+      if (error) {
+        setErrors(error.graphQLErrors)
+      } else if (data) {
+        resetErrors()
+        reset()
+      }
+    } catch (e) {
+      console.log(e)
+    }
   }
 
   return (
@@ -29,16 +65,25 @@ export const CreateTodoForm = () => {
       <Heading>Create TODO</Heading>
       <Box h={4} />
       <chakra.form onSubmit={handleSubmit(handleCreateTodo)}>
-        <FormControl label={'タイトル'} isRequired>
-          <Input />
+        <FormControl
+          label={'タイトル'}
+          isRequired
+          errorMessages={findErrorMessages('title')}
+        >
+          <Input {...register('title')} />
         </FormControl>
         <Box h={2} />
-        <FormControl label={'説明'}>
-          <Input />
+        <FormControl
+          label={'説明'}
+          errorMessages={findErrorMessages('description')}
+        >
+          <Input {...register('description')} />
         </FormControl>
         <Box h={4} />
         <Center>
-          <Button type={'submit'}>送信</Button>
+          <Button type={'submit'} isLoading={fetching}>
+            送信
+          </Button>
         </Center>
       </chakra.form>
     </Box>
